98be16a501e5dfcf32865208a1ce3d15efdc2421
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-22 14:42:53 +0200

    ja

--- www/script.js
+++ www/script.js
@@ -2,15 +2,82 @@
 browser: true,
 devel: true,
 */
-		window.onload = function () {
-			var canvas = document.getElementById("myCanvas");
-			var ctx = canvas.getContext("2d");
-			var img = document.getElementById("scream");
-			var width = document.getElementById("scream").width;
-			ctx.drawImage(img, 0, 0, 3000, 3000, 0, 0, 3000, 3000);
-			var dataURL = canvas.toDataURL();
-			document.getElementById("defImg").setAttribute('crossOrigin', 'anonymous');
-			document.getElementById("defImg").src = dataURL;
+/*
+global CanvasCamera
+*/
+var pictureSource; // picture source
+var destinationType; // sets the format of returned value 
 
-		};
-	
+// Wait for Cordova to connect with the device
+//
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready to be used!
+//
+function onDeviceReady() {
+	console.log(window.device);
+	console.log(window.plugins);
+	alert('device ready');
+	/*var objCanvas = document.getElementById("canvas");
+	window.plugin.CanvasCamera.initialize(objCanvas);
+	alert('canvas ready');*/
+	document.getElementById("takePicture").addEventListener("click", takePicture, false);
+	//document.getElementById("takePicturePreview").addEventListener("click", onTakePicture, false);
+
+}
+
+/*function onTakePicture() {
+	CanvasCamera.takePicture(onTakeSuccess);
+	alert('onTakePicture');
+		var opt = {
+		quality: 75,
+		destinationType: CanvasCamera.DestinationType.DATA_URL,
+		encodingType: CanvasCamera.EncodingType.JPEG,
+		saveToPhotoAlbum: false,
+		correctOrientation: true,
+		width: 200,
+		height: 200
+	};
+	CanvasCamera.start(opt);
+}
+
+function onTakeSuccess(data) {
+	alert('ontakesucces');
+	var image = document.getElementById("myImage");
+	image.src = "data:image/jpeg;base64," + data; // options.encodingType == CanvasCamera.EncodingType.JPEG
+	// image.src = "data:image/png;base64," + data; // options.encodingType == CanvasCamera.EncodingType.PNG
+}*/
+
+
+
+function takePicture(e) {
+	navigator.camera.getPicture(onSuccess, onFail, {
+		quality: 50,
+		destinationType: navigator.camera.DestinationType.DATA_URL,
+		allowEdit: true,
+	});
+
+}
+
+function onSuccess(imageData) {
+	//var image = document.getElementById("myImage");
+	//image.src = "data:image/jpeg;base64," + imageData;
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+	var imageObj = document.getElementById("myImage");
+	imageObj.src = "data:image/jpeg;base64," + imageData;
+	context.drawImage(imageObj, 0, 0, 3000, 3000, 0, 0, 3000, 3000);
+	var dataURL = canvas.toDataURL();
+	document.getElementById("defImg").setAttribute('crossOrigin', 'anonymous');
+	document.getElementById("defImg").src = dataURL;
+	//imageObj.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+	setTimeout(function () {
+		//alert('Failed because: ' + message);
+	}, 0);
+
+}
